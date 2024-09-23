0e08f73348004b3c525b0b63e88b1bb226fe7bf8
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-09-23 10:19:29 +0200

    messaggio di riavvio

--- installa-sway.sh
+++ installa-sway.sh
@@ -5,7 +5,4 @@ sudo sh setup-installa-sway.sh
 mkdir -p "$HOME/.config/sway/"
 cp ./swayconfig "$HOME/.config/sway/config"
 
-printf 'Riavviare adesso ? [s/N]: '
-Y="Sì"
-read Y
-printf 'Y:{%s}' "$Y"
+echo 'È necessario riavviare il sistema per confermare i cambiamenti'
